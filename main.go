51ece0b17fe38a1e94e3c085ff99378e7cce1738
post-l <ludovic.post@epitech.eu>
2016-01-08 17:49:44 +0100

    Fix initDB

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/go-martini/martini"
@@ -11,17 +12,17 @@ import (
 
 var db gorm.DB
 
-func initDB() (err error) {
+func initDB() {
+	var err error
 	user := os.Getenv("DB_ENV_MYSQL_USER")
 	password := os.Getenv("DB_ENV_MYSQL_PASSWORD")
 	dbname := os.Getenv("DB_ENV_MYSQL_DATABASE")
 	sqlConnection := fmt.Sprintf("%s:%s@tcp(db:3306)/%s?charset=utf8&parseTime=True&loc=Local", user, password, dbname)
 	db, err = gorm.Open("mysql", sqlConnection)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
-	db.AutoMigrate(&User{})
-	return
+	db.AutoMigrate(&Answer{}, &AnswerHistory{}, &Message{}, &Question{}, &User{})
 }
 
 func main() {
