b5117c549d862712a840cfcf40514704fde6b074
SlevinSadin <sadin.nicolas7@gmail.com>
2016-01-09 13:17:28 +0100

    Add refreshQuestion, nextQuestion, updateUsersPoints, randomizeQuestion

--- question.go
+++ question.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math/rand"
 	"net/http"
 	"time"
 
@@ -28,6 +29,41 @@ func getCurrentQuestion(w http.ResponseWriter, r *http.Request) {
 // GetCurrentQuestion returns the current question
 func GetCurrentQuestion() (*Question, error) {
 	question := &Question{}
-	err := db.Order("started_at desc").First(question).Error
+	err := db.Order("started_at").Last(question).Error
 	return question, err
+}
+
+func nextQuestion() error {
+	if err := updateUsersPoints(); err != nil {
+		return err
+	}
+	if err := randomizeQuestion(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateUsersPoints() error {
+	q, err := GetCurrentQuestion()
+	if err != nil {
+		return nil
+	}
+	tx := db.Begin()
+	err = tx.Exec("UPDATE `users` JOIN `answer_entries` ON users.id = answer_entries.user_id SET users.points = users.points + 1 WHERE answer_entries.question_id = ? AND answer_entries.answer_id = ?", q.ID, q.RightAnswerID).Error
+	tx.Commit()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func randomizeQuestion() error {
+	var questions []Question
+	tx := db.Begin()
+	defer tx.Commit()
+	if err := tx.Where("started_at = ?", 0).Find(&questions).Error; err != nil {
+		return err
+	}
+	question := &questions[rand.Intn(len(questions))]
+	return tx.Model(question).UpdateColumn("started_at", time.Now()).Error
 }
